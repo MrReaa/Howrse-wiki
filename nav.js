1ea302fb80bfc76224fa8dd351574ba993e07eae
Ronja Ahonen <ronja.e.a.ahonen@gmail.com>
2024-10-20 01:27:45 +0300

    nav nappi valmis

--- nav.js
+++ nav.js
@@ -3,42 +3,15 @@ fetch('topnav.html')
   .then(data => {
     document.getElementById('topnav-placeholder').innerHTML = data;
 
-    console.log('1');
-
     const currentPage = window.location.pathname.split("/").pop();
-
-    console.log(currentPage);
-    console.log('2');
-
-
     const navLinks = document.querySelectorAll('.topnav a');
-
-    console.log(navLinks);
-    console.log('3');
-
 
     navLinks.forEach(link => {
 
-      console.log('4');
-
-
       const linkPage = link.getAttribute('href');
-
-      console.log(linkPage);
-      console.log('4.1');
-
-
       if (linkPage === currentPage) {
-
-        console.log('5');
-
-
         link.classList.add('active');
       } else {
-
-        console.log('6');
-
-
         link.classList.remove('active');
       }
     });

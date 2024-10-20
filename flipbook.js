7bf15985b93aae7f74d58ccfa130867ffd35d680
Ronja Ahonen <ronja.e.a.ahonen@gmail.com>
2024-10-20 06:27:49 +0300

    Sivujen kääntö oikeaan kohtaan testi

--- flipbook.js
+++
@@ -1,8 +0,0 @@
-$(".flipbook").turn();
-
-// Handle link clicks
-$('a[data-page]').on('click', function(event) {
-    event.preventDefault(); // Prevent default anchor click behavior
-    var pageNumber = $(this).data('page'); // Get the page number from data attribute
-    $(".flipbook").turn('page', pageNumber); // Turn the flipbook to the specified page
-});
